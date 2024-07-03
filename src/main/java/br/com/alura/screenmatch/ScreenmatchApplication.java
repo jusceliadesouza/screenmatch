3cf8d7b98d7deaa778fa42b5ac235215726261b0
Juscélia de Souza <jusceliadesousa@gmail.com>
2024-07-03 20:58:48 -0300

    chore: add api data retrieval functionality

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.service.ConsumoApi;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
@@ -13,7 +15,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem Web!");
-	}
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 
+		System.out.println(json);
+
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+
+		System.out.println(json);
+	}
 }
