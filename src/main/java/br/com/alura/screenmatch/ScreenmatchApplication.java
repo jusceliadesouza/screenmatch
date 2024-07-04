eaa52a04a15083c275b33ebc1089716ba95ec890
Juscélia de Souza <jusceliadesousa@gmail.com>
2024-07-03 22:09:18 -0300

    chore: modeling series data

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,5 +24,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 		// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		// System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
